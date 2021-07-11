7bec489c8c34c4fcc78022728569566f24a7eb03
Cuong Tran <mickey123.bct@gmail.com>
2021-07-11 21:35:20 +0700

    Complete full feature of tođos app

--- reducer.js
+++ reducer.js
@@ -1,28 +1,63 @@
+import storage from './util/storage.js'
+
 const init = {
-	todos: [
-		{
-			title: 'Learn Javascript',
-			completed: true
-		},
-		{
-			title: 'Learn HTML, CSS',
-			completed: false
+	todos: storage.get(),
+	filter: 'all',
+	filters: {
+		all: () => true,
+		active: todo => !todo.completed,
+		completed: todo => todo.completed
+	},
+	editIndex: null
+}
+
+const actions = {
+	add({ todos }, title) {
+		if (title) {
+			todos.push({ title, completed: false })
+			storage.set(todos)
 		}
-	]
+	},
+	toggle({ todos }, index) {
+		todos[index].completed = !todos[index].completed
+		storage.set(todos)
+	},
+	toggleAll({ todos }, completed) {
+		todos.forEach(todo => todo.completed = completed)
+		storage.set(todos)
+	},
+	delete({ todos }, index) {
+		todos.splice(index, 1)
+		storage.set(todos)
+	},
+	switchFilter(state, filter) {
+		state.filter = filter
+	},
+	clearCompleted(state) {
+		state.todos = state.todos.filter(state.filters.active)
+		storage.set(state.todos)
+	},
+	startEdit(state, index) {
+		state.editIndex = index
+	},
+	endEdit(state, title) {
+		if (state.editIndex !== null) {
+			if (title) {
+				state.todos[state.editIndex].title = title
+				storage.set(state.todos)
+			} else {
+				this.delete(state, state.editIndex)
+			}
+			state.editIndex = null
+		}
+	},
+	cancelEdit(state) {
+		state.editIndex = null
+	}
 }
 
 export default function reducer(state = init, action, args) {
-	switch (action) {
-		case 'add':
-			const [title] = args
-			return {
-				...state,
-				todos: [...state.todos, {
-					title,
-					completed: false
-				}]
-			}
-		default:
-			return state
-	}
+	actions[action] && actions[action](state, ...args)
+	
+	return state
 }
